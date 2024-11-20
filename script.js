e9b281db6d31c4294fc405747122c0110dbcd078
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-11-20 09:15:47 -0300

    Retirada do botão fechar

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 var botao_barra_lateral = document.getElementById("barra-lateral-btn")
-var botao_barra_lateral_fechar = document.getElementById("botao-fechar")
 
 botao_barra_lateral.addEventListener("click", () => {
-    var sidebar = document.getElementById("barra-lateral-div");
-
-    sidebar.classList.toggle("open");
-})
-
-botao_barra_lateral_fechar.addEventListener("click", () => {
     var sidebar = document.getElementById("barra-lateral-div");
 
     sidebar.classList.toggle("open");
