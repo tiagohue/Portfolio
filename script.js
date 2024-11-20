c5ac4c675e5349c9b934d9e06414cdc8ab43b867
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-11-20 11:32:51 -0300

    Revert "Retirada do botão fechar"
    This reverts commit e9b281db6d31c4294fc405747122c0110dbcd078.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var botao_barra_lateral = document.getElementById("barra-lateral-btn")
+var botao_barra_lateral_fechar = document.getElementById("botao-fechar")
 
 botao_barra_lateral.addEventListener("click", () => {
+    var sidebar = document.getElementById("barra-lateral-div");
+
+    sidebar.classList.toggle("open");
+})
+
+botao_barra_lateral_fechar.addEventListener("click", () => {
     var sidebar = document.getElementById("barra-lateral-div");
 
     sidebar.classList.toggle("open");
